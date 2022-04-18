fda30db6c83236fb11dd8a3fd11f676b4da0da2f
Lucas Anjos <42415782+luskas8@users.noreply.github.com>
2022-04-18 20:12:33 -0300

    [FEATURE] - código para apresentação

--- V3.pl
+++ V3.pl
@@ -1,22 +1,14 @@
 /*
 
-Obs's.: 
-
-- Implementar de ter tipo de trabalho remoto ou presencial.
-
 Perguntas meta:
 
-1.	Quais candidatos estão aptos a participar do processo seletivo?
-2.	Quais as cidades de moradia dos candidatos?
-3.	Quais candidatos estão na cidade da empresa recrutadora?
+1.	Quais as cidades de moradia dos candidatos?
+2.	Qual a faixa etária dos candidatos?
+3.	O regime de contratação é compatível com o regime de contratação pretendido pelos candidatos?
 4.	A faixa de salário oferecida é compatível com as pretendidas?
-5.	Qual a faixa etária dos candidatos?
-6.	O regime de contratação é compatível com o regime de contratação pretendido pelos candidatos?
+5.	Quais candidatos estão aptos a participar do processo seletivo?
+6.	Quais candidatos estão na cidade da empresa recrutadora?
 7.	Quais os melhores candidatos para determinada vaga? Justifique.
-
-senioridade(senior, 3)
-senioridade(pleno, 2)
-senioridade(junior, 1)
 
 */
 
@@ -28,73 +20,46 @@ candidate(teste, cmaismais, pleno, 2000, cidadeB, 22, clt).
 candidate(marcelo, java, senior, 3000, cidadeC, 24, clt).
 candidate(luiz, cmaismais, pleno, 1500, cidadeA, 26, clt).
 candidate(piva, java, senior, 4000, cidadeB, 28, clt).
+candidate('bob esponja', python, senior, 4000, cidadeB, 28, clt).
+candidate(antonio, python, senior, 4000, cidadeB, 28, pj).
 
 /* 7 atributos */
 /*  nome_da_empresa, cidade_da_empresa, forma_de_trabalho, salario_esperado, nível_de_qualificação, linguagem_esperada, tipo_de_contratação_esperada */
-company(empresaA, cidadeA, remoto, 1500, junior, python, pj).
-company(empresaB, cidadeC, remoto, 4000, junior, java, clt).
-company(empresaC, cidadeB, presencial,8000, pleno, python, clt).
+company(empresaA, cidadeA, remoto, 4000, junior, python, pj).
+company(empresaB, cidadeC, remoto, 4000, pleno, java, clt).
+company(empresaC, cidadeB, presencial, 8000, senior, python, clt).
 
 /*
- * 1.	Quais candidatos estão aptos a participar do processo seletivo? GUSTAVO
- */
-
-/*Obs.: Implementar o nivel de qualificação como nivel_de_qualificacao_esperado >= nivel_de_qualificacao
-*/
-
-/* Os candidatos aptos a participar do processo seletivo devem atender os seguintes parâmetros:
- * salario_esperado <= salario_esperado_empresa
- * nivel_de_qualificacao_esperado = nivel_de_qualificacao
- * tipo_de_contratação = tipo_de_contratação_esperada
- * */
-
-/*seleciona a qualificação de cada candidato*/
-match_qualificacao(COMPANY, CANDIDATE, QUALIFICACAO_COMPANY, _QUALIFICACAO_CANDIDATE) :-
-	company(COMPANY, _, _, _, QUALIFICACAO_COMPANY, _, _),
-	candidate(CANDIDATE, _, _QUALIFICACAO, _, _, _, _).
-
-/*seleciona as melhores opções para o processo seletivo para a empresa X*/
-best_option_processo_seletivo(COMPANY, CANDIDATE, CONTRATACAO, QUALIFICACAO, EXPECTED_SALARY) :-
-    match_contratacao(COMPANY, CANDIDATE, CONTRATACAO),
-    match_qualificacao(COMPANY, CANDIDATE, QUALIFICACAO),
-    salary_lower(CANDIDATE, EXPECTED_SALARY).
-
-/*Mostra melhor escolha para o processo seletivo da empresa X*/
-show_best_choice_processo_seletivo_company(COMPANY) :-
-    company(COMPANY, _, _, EXPECTED_SALARY, _, _, _),
-    best_option_processo_seletivo(COMPANY, CANDIDATE, _CONTRATACAO, _QUALIFICACAO, EXPECTED_SALARY),
-    format('~w Melhor escolha ~w\n',[CANDIDATE, COMPANY]).
-
-/*Mostra melhores escolhas para o processo seletivo para a empresa X*/
-gshow_best_choice_processo_seletivo_company(Result) :-
-    company(COMPANY, _, _, EXPECTED_SALARY, _, _, _),
-    findall(CANDIDATE, best_option_processo_seletivo(COMPANY, CANDIDATE, _CONTRATACAO, _QUALIFICACAO, EXPECTED_SALARY), R),
-    sort(R, Result),
-    format('~w Melhores escolhas ~w', [COMPANY, Result]).
-
-/*
- * 2.	Quais as cidades de moradia dos candidatos? LUCAS
+ * 1.	Quais as cidades de moradia dos candidatos?
  */
 get_candidate_city(Result) :-
     findall(CITY, candidate(_, _, _, _, CITY, _, _), R), sort(R, Result),
     format('Cidades dos candidados: ~w\n', [Result]).
 
 /*
- * 3.	Quais candidatos estão na cidade da empresa recrutadora? LUCAS
+ * 2.	Qual a faixa etária dos candidatos?
  */
-is_remote(COMPANY, OUT) :-
-	company(COMPANY, _, IsREMOTE, _, _, _, _),
-	(IsREMOTE = 'remoto', OUT=1; IsREMOTE \= 'remoto', OUT=2).
 
-match_city(COMPANY) :-
-    is_remote(COMPANY, OUT), OUT = 1, candidate(CANDIDATE, _, _, _, _, _, _),
-    format('~w ~w', [COMPANY, CANDIDATE]);
-    is_remote(COMPANY, OUT), OUT = 2, company(COMPANY, CITY, _, _, _, _, _),  candidate(CANDIDATE, _, _, _, CITY, _, _),
-    format('~w ~w', [COMPANY, CANDIDATE]).
-    /*msort(R, Result),*/
+get_canditates_ages(Result) :-
+    findall(AGE, candidate(_, _, _, _, _, AGE, _), R), sort(R, Result),
+    format('Idade dos candidados: ~w\n', [Result]).
 
 /*
- * 4.	A faixa de salário oferecida é compatível com as pretendidas? LUCAS
+ * 3.	O regime de contratação é compatível com o regime de
+ * contratação pretendido pelos candidatos?
+ * 
+ * clt/pj
+ */
+match_contratacao(EMPRESA, CANDIDATE, CONTRATACAO) :-
+    company(EMPRESA, _, _, _, _, _,CONTRATACAO),
+	candidate(CANDIDATE, _, _, _, _, _, CONTRATACAO).
+
+show_match_contratacao :-
+    match_contratacao(EMPRESA, CANDIDATE, CONTRATACAO),
+    format('~w match ~w (~w)', [CANDIDATE, EMPRESA, CONTRATACAO]).
+
+/*
+ * 4.	A faixa de salário oferecida é compatível com as pretendidas?
  */
 salary_lower(CANDIDATE, EXPECTED_SALARY) :-
     candidate(CANDIDATE, _, _, SALARY, _, _, _),
@@ -107,30 +72,49 @@ salary_company_expected(COMPANY, Result) :-
     format('Esses candidatos tem expectatvas de sálario,\ncompatíveis com a empresa ~w:\n~w', [COMPANY, Result]).
 
 /*
- * 5.	Qual a faixa etária dos candidatos? GUSTAVO
- */
+ * 5.	Quais candidatos estão aptos a participar do processo seletivo?
+ *
+ * Os candidatos aptos a participar do processo seletivo devem atender os seguintes parâmetros:
+ * salario_esperado <= salario_esperado_empresa
+ * nivel_de_qualificacao_esperado = nivel_de_qualificacao
+ * tipo_de_contratação = tipo_de_contratação_esperada
+ * */
 
-get_canditates_ages(Result) :-
-    findall(AGE, candidate(_, _, _, _, _, AGE, _), R), sort(R, Result),
-    format('Idade dos candidados: ~w\n', [Result]).
-    
+match_qualification(COMPANY, CANDIDATE, QUALIFICATION) :-
+	company(COMPANY, _, _, _, QUALIFICATION, _, _),
+	candidate(CANDIDATE, _, QUALIFICATION, _, _, _, _).
+
+/*seleciona as melhores opções para o processo seletivo para a empresa X*/
+best_option_selective_process(COMPANY, CANDIDATE, CONTRATACAO, QUALIFICATION, EXPECTED_SALARY) :-
+    match_contratacao(COMPANY, CANDIDATE, CONTRATACAO),
+    match_qualification(COMPANY, CANDIDATE, QUALIFICATION),
+    salary_lower(CANDIDATE, EXPECTED_SALARY).
+
+/*Mostra melhores escolhas para o processo seletivo para a empresa X*/
+gshow_best_choice_selective_process_company(Result) :-
+    company(COMPANY, _, _, EXPECTED_SALARY, _, _, _),
+    findall(CANDIDATE, best_option_selective_process(COMPANY, CANDIDATE, _CONTRATACAO, _QUALIFICATION, EXPECTED_SALARY), R),
+    sort(R, Result),
+    format('~w Melhores escolhas ~w', [COMPANY, Result]).
+
 /*
- * 6.	O regime de contratação é compatível com o regime de
- * contratação pretendido pelos candidatos? LUCAS
+ * 6.	Quais candidatos estão na cidade da empresa recrutadora?
  */
-match_contratacao(EMPRESA, CANDIDATE, CONTRATACAO) :-
-    company(EMPRESA, _, _, _, _, _,CONTRATACAO),
-	candidate(CANDIDATE, _, _, _, _, _, CONTRATACAO).
+is_remote(COMPANY, OUT) :-
+	company(COMPANY, _, IsREMOTE, _, _, _, _),
+	(IsREMOTE = 'remoto', OUT=1; IsREMOTE \= 'remoto', OUT=2).
 
-show_match_contratacao :-
-    match_contratacao(EMPRESA, CANDIDATE, CONTRATACAO),
-    format('~w match ~w (~w)', [CANDIDATE, EMPRESA, CONTRATACAO]).
+match_city(COMPANY) :-
+    is_remote(COMPANY, OUT), OUT = 1, candidate(CANDIDATE, _, _, _, _, _, _),
+    format('~w ~w', [COMPANY, CANDIDATE]);
+    is_remote(COMPANY, OUT), OUT = 2, company(COMPANY, CITY, _, _, _, _, _),  candidate(CANDIDATE, _, _, _, CITY, _, _),
+    format('~w ~w', [COMPANY, CANDIDATE]).
 
 /*
  * 7.	Quais os melhores candidatos para determinada vaga? Justifique.
  * 
  * salário					<=		oferecido empresa,	ok
- * (remote ou presencial)	match	empresa				-
+ * (remote ou presencial)	match	empresa				ok
  * linguagem				match	empresa,			ok
  * contratação				match	empresa,			ok
  */
